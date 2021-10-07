48ab00ea0bbcd320a0a359c813478abc6a234597
harpreetattri2020 <Harpreetattri2020@gmail.com>
2021-10-07 15:31:39 +0100

    Designing front end

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="bg-black w-100% h-20 flex-shrink flex-grow ">
+        
+        <h1 className=" text-xl font-bold  pt-5 text-white   w-1/3 p-2 flex items-center justify-center  ">Photo Album App</h1>
+        
+        </div>
+    )
+}
+
+export default Header
